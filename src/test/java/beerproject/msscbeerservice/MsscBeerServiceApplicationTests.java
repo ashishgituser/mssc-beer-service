37f96d16f7d3ceaf20eb52210e12a0bfcecef06c
Ashish <ashish.chaudhary008@gmail.com>
2021-12-17 15:27:35 +0530

    Initial project setup

---
+++ src/test/java/beerproject/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package beerproject.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
